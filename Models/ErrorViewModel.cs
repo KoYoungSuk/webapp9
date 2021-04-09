2e103ca92f98eb97c75689371c469d359b863860
KoYoungSuk <vheh5678@naver.com>
2021-04-09 15:35:16 +0900

    github test for webapp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VSCODE_GIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
